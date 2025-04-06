d92490fa10b70445e1761519f739e5dba8a02295
mabendeh <Mhammad.zizia@gmail.com>
2025-04-06 17:22:07 -0500

    Update customer_complaints.py

--- customer_complaints.py
+++ customer_complaints.py
@@ -1,1 +1,10 @@
-# customer complaints module
+class CustomerComplaint:
+    def __init__(self, date, customer_name, complaint_description, corrective_action, status, owner, closure_date):
+        self.complaint_id = str(uuid.uuid4())[:8]
+        self.date = date
+        self.customer_name = customer_name
+        self.complaint_description = complaint_description
+        self.corrective_action = corrective_action
+        self.status = status
+        self.owner = owner
+        self.closure_date = closure_date
